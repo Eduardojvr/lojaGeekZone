60dbbb19716034ffa0aa9e110d57986517de4fc4
Eduardojvr <eduardojvr10@gmail.com>
2020-01-12 14:51:57 -0300

    Modifica filtro

--- geekZone/src/main/java/com/geekzone/rest/RESTUsuario.java
+++ geekZone/src/main/java/com/geekzone/rest/RESTUsuario.java
@@ -2,6 +2,7 @@ package com.geekzone.rest;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -10,6 +11,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import com.geekzone.dao.UsuarioDAO;
 import com.geekzone.entity.Usuario;
@@ -59,13 +61,50 @@ public class RESTUsuario {
 
 			if (login != null) {
 				request.getSession().setAttribute("logado", true);
-				request.getSession().setAttribute("usuario", login);
+				request.getSession().setAttribute("nomeCompleto", login.getNomeCompleto());
+				request.getSession().setAttribute("cpf", login.getCpf());
+				request.getSession().setAttribute("nascimento", login.getNascimento());
+				request.getSession().setAttribute("sexo", login.getSexo());
+				request.getSession().setAttribute("cep", login.getCep());
+				request.getSession().setAttribute("cidade", login.getBairro());
+				request.getSession().setAttribute("bairro", login.getBairro());
+				request.getSession().setAttribute("estado", login.getEstado());
+				request.getSession().setAttribute("numero", login.getNumero());
+				request.getSession().setAttribute("complemento", login.getComplemento());
+				request.getSession().setAttribute("telefone", login.getTelefone());
+				request.getSession().setAttribute("celular", login.getCelular());
+				request.getSession().setAttribute("email", login.getEmail());
+			} else {
+				return Response.serverError().entity("Erro ao logar!").build();	
 			}
 		}catch(Exception e) {
 			e.printStackTrace();
-			return Response.serverError().entity("Erro ao logar!").build();	
 		}
 		return Response.ok().entity("Usuário logado com sucesso!").build();	
+	}
+	
+	@POST
+	@Path("/logout")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response logout() {
+		HttpSession session = request.getSession();
+		String firstRedirectUrl = request.getContextPath()+"/index.html";
+		try {
+			if (session != null) {
+				request.getSession().setAttribute("logado", false);
+//				session.invalidate();
+				response.sendRedirect(firstRedirectUrl);
+				return Response.ok().status(Status.OK).build();
+			} else {
+				return Response.status(Status.FORBIDDEN).build();
+			}
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return Response.status(Status.FORBIDDEN).entity("Erro ao realizar logout!").build();
+		}
+
 	}
 }
 
